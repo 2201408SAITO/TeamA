2bdd8f747632f7298078a2afb2f841895ca9ce3c
2201418Terasaki <2201418@s.asojuku.ac.jp>
2023-11-21 12:56:56 +0900

    Create buycomp.php

---
+++ User/buycomp.php
@@ -0,0 +1,27 @@
+<?php require 'header.php'; ?>
+<?php require 'menu.php'; ?>
+<?php require 'db-connect.php';?>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品登録完了画面</title>
+    <link rel="stylesheet" href="css/Finish.css">
+    <script src="./script/Register.js"></script>
+</head>
+<body>
+
+   
+    <h2>ご購入ありがとうございました</h2>';
+              
+       
+        <section class="foot">
+            <form action="index.php" method="post">
+                <button class="register" type="submit">HOME</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
