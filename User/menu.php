8b65462ada0680c57817d9d9078fb6f89a6bad91
2201418Terasaki <2201418@s.asojuku.ac.jp>
2023-11-22 18:09:19 +0900

    memu-add-mypage
    create mypage and added  button to login and logout in maypage

--- User/menu.php
+++ User/menu.php
@@ -29,7 +29,7 @@
     <div class="level-right">
     <div class="level-item"><a href="index.php" ><i class="fas fa-home fa-3x" style="color:dimgray;"></i></a></div>
     <div class="level-item"><a href="cart-show.php" ><i class="fas fa-shopping-cart fa-3x" style="color:dimgray"></i></a></div>
-    <div class="level-item"><a href="login-input.php" ><i class="far fa-user-circle fa-3x" style="color:dimgray"></i></a></div>
+    <div class="level-item"><a href="mypage.php" ><i class="far fa-user-circle fa-3x" style="color:dimgray"></i></a></div>
     </div>
 </nav>
 <hr>
