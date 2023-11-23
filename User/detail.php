828c6ece89389dae1ec872c4fbad5b8639b385d1
2201418Terasaki <2201418@s.asojuku.ac.jp>
2023-11-24 04:30:33 +0900

    a

--- User/detail.php
+++ User/detail.php
@@ -1,40 +1,86 @@
-<?php require 'header.php'; ?>
-<?php require 'menu_noswip.php'; ?>
-<?php require 'db-connect.php';?>
-<?php
-// カテゴリーIDと対応するカテゴリー名のマッピング
-$categoryMapping = [
-    1 => 'テレビ',
-    2 => 'ゲーム',
-    3 => '家電',
-    4 => '靴',
-    5 => 'おもちゃ',
-    6 => 'スマートフォン',
-    7 => '服',
-    8 => '靴',
-    9 => '本',
-    10 => '家具',
-];
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+    <meta http-equiv="Cache-Control" content="no-cache">
+        <meta charset="UTF-8">   
+        <title>商品更新画面</title>
+        <link rel="stylesheet" href="css/detail.css">
+    </head>
 
-$sql=$pdo->prepare('select * from goods where goods_id=?');
-$sql->execute([$_GET['id']]);
-foreach ($sql as $row) {
-    echo '<p><img alt="image" src="image/', $row['goods_id'], '.jpg" width="400" height="400"></p>';
-    echo '<form action="cart-insert.php" method="post">';
-    echo '<p>カテゴリー:', $categoryMapping[$row['category_id']], '</p>';
-    echo '<p>商品名:', $row['goods_name'], '</p>';
-    echo '<p>販売価格:￥', $row['price'], '</p>';
-    echo '<p>商品説明:', $row['exp'], '</p>';
-    echo '<p>数量:<select name="count">';
-    for ($i=1; $i<=10; $i++) {
-        echo '<option value="', $i, '">', $i, '</option>';
-    }
-    echo '</select></p>';
-    echo '<input type="hidden" name="id" value="', $row['goods_id'], '">';
-    echo '<input type="hidden" name="name" value="', $row['goods_name'], '">';
-    echo '<input type="hidden" name="price" value="', $row['price'], '">';
-    echo '<p><input type="submit" value="カートイン"></p>';
-    echo '</form>';
-}
-?>
-<?php require 'footer.php'; ?>
+    <body>
+    <?php require 'header.php'; ?>
+<?php require 'menu_noswip.php'; ?>
+        <div class="wrapper">
+       
+            <section class="head">
+                <h2>商品詳細</h2>
+            </section>
+            <?php
+                 require 'db-connect.php';
+               
+                $l = "location.href='index.php'";
+                $file = "fileInput";
+                $s = "this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g,'$1');";
+       
+                $sql=$pdo->prepare('select goods. * , category_name from goods inner join categories on goods.category_id = categories.category_id where goods_id=?');
+                $sql->execute([$_GET['id']]);
+                foreach($sql as $row){
+                    echo '<form action = "cart-insert.php" method = "post" enctype="multipart/form-data class="formbox"">';
+                    echo     '<input type="hidden" name="id" value="'.$row['goods_id'].'">';
+                    echo     '<input type="hidden" name="OldCategory" value="'.$row['category_id'].'">';
+                    echo     '<input type="hidden" name="OldName" value="'.$row['goods_name'].'">';
+                    echo     '<section class="body">';
+                    echo         '<div class="image">';
+                    echo             '<label>画像：</label>';
+                    $category=$row['category_name'];
+                    $id=$row['goods_name'];
+                    $imageDirectory = '../manager/img/' . $category . '/'.$id.'/';
+                    $images = glob($imageDirectory . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+                    if (!empty($images)) {
+                        foreach ($images as $image) {
+                            $fileName = basename($image);
+                        echo '<img src="' . $image . '" class="UpdatedImages" alt="' . $fileName . '" width="65" height="65">';
+                        }
+                    }else{
+                        echo 'No images';
+                    }
+       
+                    echo         '</div>';
+                 
+                    echo         '<div>';
+                    echo         '<label>カテゴリ：</label>'.$row['category_name'];
+                    echo         '</div>';
+                    echo         '<div>';
+                    echo             '<label>商品名：</label>'.$row['goods_name'];
+                    echo         '</div>';
+                    echo         '<div>';
+                    echo             '<label>販売単価：</label>'.$row['price'].'円';
+                    echo         '</div>';
+                    echo         '<div class="explain">';
+                    echo             '<label>商品説明：</label>'.$row['exp'];
+                    echo         '</div>';
+                    echo '<div>';
+                    echo '<label>個数：</label>';
+                    echo '<select class="input-box-number" style="padding: 5px;" required="required" name="count">';
+                    for ($i = 1; $i <= 20; $i++) {
+                       
+                        echo '<option value="' . $i . '">' . $i . '</option>';
+                    }
+                    echo '</select>個';
+                    echo '</div>';
+                    echo     '</section>';
+                    echo     '<section class="foot">';
+                    echo '<input type="hidden" name="id" value="', $row['goods_id'], '">';
+                    echo '<input type="hidden" name="name" value="', $row['goods_name'], '">';
+                    echo '<input type="hidden" name="price" value="', $row['price'], '">';
+                    echo         '<input type="button" value="戻る" class="register" onclick="'.$l.'">';
+                    echo         '<button class="register" type="submit">カートイン</button>';
+                    echo     '</section>';
+                    echo '</form>';
+                }
+            ?>
+        </div>
+     
+    </body>
+</html>
+ 
