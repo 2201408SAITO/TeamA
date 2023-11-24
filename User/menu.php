4088b99e9741a7f18c1bad604cfe859ad61e4f6b
2201408SAITO <2201408@s.asojuku.ac.jp>
2023-11-24 12:28:44 +0900

    Update menu.php

--- User/menu.php
+++ User/menu.php
@@ -6,7 +6,7 @@
     <div class="select is-normal">
     <select name="category">
     <option value="">カテゴリー</option>
-        <option value="1">テレビ</option>
+        <option value="1">家具</option>
         <option value="2">ゲーム</option>
         <option value="3">家電</option>
         <option value="4">靴</option>
@@ -14,7 +14,7 @@
         <option value="6">スマートフォン</option>
         <option value="7">服</option>
         <option value="8">本</option>
-        <option value="9">家具</option>
+   
     </select>
     </div>
     <div class="control">
@@ -30,6 +30,7 @@
     <div class="level-item"><a href="index.php" ><i class="fas fa-home fa-3x" style="color:dimgray;"></i></a></div>
     <div class="level-item"><a href="cart-show.php" ><i class="fas fa-shopping-cart fa-3x" style="color:dimgray"></i></a></div>
     <div class="level-item"><a href="mypage.php" ><i class="far fa-user-circle fa-3x" style="color:dimgray"></i></a></div>
+    <div class="level-item"><a href="login-input.php" ><i class="far fa-user-circle fa-3x" style="color:dimgray"></i></a></div>
     </div>
 </nav>
 <hr>
