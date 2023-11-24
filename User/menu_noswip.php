2adc24f553081aac7d63e92808975184a8e28924
NagaeRyuto <2201374@s.asojuku.ac.jp>
2023-11-24 12:36:45 +0900

    comit

--- User/menu_noswip.php
+++ User/menu_noswip.php
@@ -1,12 +1,13 @@
 <?php require 'header.php'; ?>
-<form action="index.php" method="post" class="has-background-grey-lighter">
+<div class="aaa">
+<form action="index.php" method="post" class="has-background-success-light">
 <nav class="level">
-<img src="img/logo.png" class="logo" alt="" width="100" height="85">
+<a href="index.php" ><img src="img/logo.png" class="logo" alt="" width="100" height="85"></a>
     <div class="level-item">
     <div class="select is-normal">
-    <select name="category">
+    <select name="category" style="border-radius: 25px; outline:none; border-color:seagreen; margin-right:5px;">
     <option value="">カテゴリー</option>
-        <option value="1">テレビ</option>
+        <option value="1">家具</option>
         <option value="2">ゲーム</option>
         <option value="3">家電</option>
         <option value="4">靴</option>
@@ -14,11 +15,11 @@
         <option value="6">スマートフォン</option>
         <option value="7">服</option>
         <option value="8">本</option>
-        <option value="9">家具</option>
+   
     </select>
     </div>
     <div class="control">
-    <input type="text" name="keyword" class="input is-normal " type="text" placeholder="Search">
+    <input type="text" name="keyword" class="input is-normal" style="border-radius: 25px; outline:none; border-color:seagreen; width:500px; padding: 0 50px 0 10px;" length="" type="text" placeholder="Search">
     </div>
     <button class="button">
         <span class="icon is-small">
@@ -27,10 +28,36 @@
     </button>
     </div>
     <div class="level-right">
-    <div class="level-item"><a href="index.php" ><i class="fas fa-home fa-3x" style="color:dimgray;"></i></a></div>
-    <div class="level-item"><a href="cart-show.php" ><i class="fas fa-shopping-cart fa-3x" style="color:dimgray"></i></a></div>
-    <div class="level-item"><a href="mypage.php" ><i class="far fa-user-circle fa-3x" style="color:dimgray"></i></a></div>
+    <div class="level-item"><a href="index.php" ><i class="fas fa-home fa-3x" style="color:seagreen;"></i></a></div>
+    <div class="level-item"><a href="cart-show.php" ><i class="fas fa-shopping-cart fa-3x" style="color:seagreen"></i></a></div>
+    <div class="level-item"><a href="login-input.php" ><i class="far fa-user-circle fa-3x" style="color:seagreen"></i></a></div>
     </div>
 </nav>
-<hr>
 </form>
+</div>
+
+<style>
+  .control{
+
+  }
+  
+    .button{
+      display: flex;
+      right:40px;
+      border-color:seagreen;
+    }
+    .logo{
+      margin-left:20px ;
+    }
+
+    .level-right{
+      margin-right:20px;
+    }
+    .aaa{
+      position: fixed;
+      top:0;
+      z-index: 999;
+      width:100%;
+    }
+ 
+</style>
