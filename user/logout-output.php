3c8b3af5df1cb07c47b41331aa6d2e7c226edc70
2201408SAITO <2201408@s.asojuku.ac.jp>
2023-11-27 11:01:59 +0900

    Update logout-output.php

--- user/logout-output.php
+++ user/logout-output.php
@@ -6,15 +6,30 @@
 <?php
 if(isset($_SESSION['users'])){
     unset($_SESSION['users']);
+
+    echo '<div class ="aaa">';
+    echo '<div class ="wrapper">';
+    echo '<div class ="out">';
+
     echo 'ログアウトしました。';
+    echo '<form method="POST" action="login-input.php">';
+    echo '<button type="submit" class="btn">ログイン画面へ</button>';
+    echo '</form>';
 
   
 }else{
+
+    echo '<div class ="aaa">';
+    echo '<div class ="wrapper">';
+    echo '<div class ="out">';
+
     echo 'すでにログアウトしています。';
+    echo '<form method="POST" action="login-input.php">';
+    echo '<button type="submit" class="btn">ログイン画面へ</button>';
+    echo '</form>';
+
+    
     
 }
-echo '<form method="POST" action="login-input.php">';
-echo '<button type="submit" class="btn">ログイン画面へ</button>';
-echo '</form>';
 ?>
 <?php require 'footer.php'; ?>
