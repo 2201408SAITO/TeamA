f452aa7728fd14c9c0e9cdd8602129429fdc0881
2201418Terasaki <2201418@s.asojuku.ac.jp>
2023-11-27 14:50:06 +0900

    a

--- User/buymethods.php
+++ User/buymethods.php
@@ -1,4 +1,11 @@
 <?php session_start(); ?>
+<?php
+if (!isset($_SESSION['users']['id'])) {
+    header('Location: https://aso2201418.vivian.jp/GitHub/TeamA/User/login-input.php');
+    exit(); // これ以降のコードを実行しない
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
