7a6ae52fbfaa5064b30d987074142091ec6ec5f6
2201408SAITO <2201408@s.asojuku.ac.jp>
2023-11-27 10:02:21 +0900

    cart total修正

--- User/cart.php
+++ User/cart.php
@@ -19,12 +19,14 @@ if (!isset($_SESSION['users']['id'])) {
 }
 $user_id = $_SESSION['users']['id'];
 
+$total = 0; //カートの合計金額
+
 if (!empty($_SESSION['user_cart'][$user_id])) {
     echo '<section class="body">';
     echo '<table>';
     echo '<tr><th>商品画像</th><th></th><th>商品名</th><th></th>';
     echo '<th>個数</th><th></th><th>価格</th><th></th><th></th></tr>';
-    $total = 0;
+    
     echo '<form action="buymethods.php" method="POST">'; 
 
     foreach ($_SESSION['user_cart'][$user_id] as $id => $product) {
