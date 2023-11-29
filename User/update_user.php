e43c562b1cff717cb2903462b393ee99b90d426b
NagaeRyuto <2201374@s.asojuku.ac.jp>
2023-11-29 15:30:12 +0900

    comit

---
+++ User/update_user.php
@@ -0,0 +1,57 @@
+<?php session_start(); ?>
+<?php require 'header.php'; ?>
+<?php require 'menu_noswip.php'; ?>
+<?php require 'db-connect.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Mypage</title>
+    <link rel="stylesheet" href="css/update_user.css"> 
+</head>
+<body>
+<br><br><br><br><br>
+<div class="main">ユーザー情報</div>
+<br>
+<?php
+    // ログインしているユーザーの情報を取得する
+    $sql = $pdo->prepare('SELECT * FROM users WHERE user_id = ?');
+    $sql->execute([$_SESSION['users']['id']]);
+    
+    // 取得したユーザー情報を表示する
+    $row = $sql->fetch(PDO::FETCH_ASSOC);
+    echo  '<p>',$_SESSION['users']['name'], 'さんのマイページ<p>';
+    echo '<hr>';
+    $name=$post_code=$address=$phone_number=$mail='';
+    if(isset($_SESSION['users'])){
+        $name=$_SESSION['users']['name'];
+        $post_code=$_SESSION['users']['post_code'];
+        $address=$_SESSION['users']['address'];
+        $phone_number=$_SESSION['users']['phone_number'];
+        $mail=$_SESSION['users']['mail'];
+    }
+    echo '<form action="update_user_output.php" method="post">';
+    echo '<p>名前　　　　　　';
+    echo '<input type="text" name="name" class="input is-normal" style="outline:none; border-color:seagreen; width:500px;" value="',$name,'">';
+    echo '</p>';
+    echo '<p>郵便番号　　　　';
+    echo '<input type="number" name="post_code" class="input is-normal" style="outline:none; border-color:seagreen; width:500px;" value="',$post_code,'">';
+    echo '</p>';
+    echo '<p>住所　　　　　　';
+    echo '<input type="text" name="address" class="input is-normal" style="outline:none; border-color:seagreen; width:500px;" value="',$address,'">';
+    echo '</p>';
+    echo '<p>電話番号　　　　';
+    echo '<input type="number" name="phone_number" class="input is-normal" style="outline:none; border-color:seagreen; width:500px;" value="',$phone_number,'">';
+    echo '</p>';
+    echo '<p>メールアドレス　';
+    echo '<input type="mail" name="mail" class="input is-normal" style="outline:none; border-color:seagreen; width:500px;" value="',$mail,'">';
+    echo '</p>';
+    echo '<hr>';
+
+    echo '<div class="but">';
+    echo '<form action="mypage.php" method="post">';
+    echo '<input type="hidden" name="user_id" value="', $row['user_id'], '">';
+    echo '<input type="submit" class="btn" value="更新">';
+    echo '</form>';
+?>
+<?php require 'footer.php'; ?>
---
+++ User/update_user_output.php
@@ -0,0 +1,49 @@
+<?php session_start(); ?>
+<?php require 'header.php'; ?>
+<?php require 'menu_noswip.php'; ?>
+<?php require 'db-connect.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Mypage</title>
+    <link rel="stylesheet" href="css/update_user_out.css"> 
+</head>
+<body>
+<br><br><br>
+<?php
+$pdo=new PDO($connect,USER,PASS);
+if(isset($_SESSION['users'])){
+    $id=$_SESSION['users']['id'];
+    $sql=$pdo->prepare('select * from users where user_id!=? and mail=?');
+    $sql->execute([$id,$_POST['mail']]);
+}else{
+    $sql=$pdo->prepare('select * from users where mail=?');
+    $sql->execute([$_POST['mail']]);   
+}
+if(empty($sql->fetchAll())){
+        if(isset($_SESSION['users'])){
+            $sql=$pdo->prepare('update users set user_name=?, post_code=?, address=?, phone_number=?, mail=? where user_id=?');
+
+            $sql->execute([
+                $_POST['name'],$_POST['post_code'],$_POST['address'],
+                $_POST['phone_number'],$_POST['mail'],$id]);
+            $_SESSION['users']=[
+                'id'=>$id,'name'=>$_POST['name'],'post_code'=>$_POST['post_code'],
+                'address'=>$_POST['address'],'phone_number'=>$_POST['phone_number'],'mail'=>$_POST['mail']
+            ];
+            echo '<div class ="aaa">';
+            echo '<div class ="wrapper">';
+            echo '<div class ="out">';
+            echo '<form method="POST" action="mypage.php">';
+            echo '<div class="mes" style="text-align: center;">ユーザー情報を更新しました。';
+            echo '<button type="submit" class="btn">マイページ</button>';
+            echo '</form>';
+            echo '</div>';  
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+        }
+}
+?>
+<?php require 'footer.php'; ?>
