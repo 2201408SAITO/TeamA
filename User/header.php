02cc076cb9406b5caf41f123ba54e7cec3c60ed1
2201408SAITO <2201408@s.asojuku.ac.jp>
2023-12-08 12:31:37 +0900

    Update header.php

--- User/header.php
+++ User/header.php
@@ -7,6 +7,6 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
-<title>PHP Sample Programs</title>
+<title>Goods Now</title>
 </head>
 <body>
